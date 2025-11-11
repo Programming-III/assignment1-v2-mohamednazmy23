096c19ffa8aef8128937ca1a067b73c6809fa99e
mohamednazmy23 <71565572+mohamednazmy23@users.noreply.github.com>
2025-11-11 09:39:31 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -22,6 +22,16 @@ void Course(string c,string cn,int m,int c,Student* students);
 void addStudent();
 void displayCourseInfo();
 
+string getcourseCode();
+void setcourseCode();
+string getcourseName();
+void setcourseName();
+int getmaxStudents();
+void setmaxStudents();
+
+
+int getcurrentStudent();
+void  setcurrentStudent();
 
 
 
