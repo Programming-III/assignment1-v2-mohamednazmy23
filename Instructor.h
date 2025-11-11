b58deb762f89d3fbe85d76fe8e8e85a39acd62e5
mohamednazmy23 <71565572+mohamednazmy23@users.noreply.github.com>
2025-11-11 09:14:03 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -6,7 +6,18 @@ using namespace std;
 //#write Instructor class here
 
 
+private :
+string department;
+int experienceYears;
 
+public:
+void Instructor();
+~  Instructor();
+
+
+void Instructor(int  i , string n,string d,int e);
+
+void display();
 
 
 
